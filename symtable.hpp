a43b6af5e9f5b20d0a3172d282e9f4279d582643
Prinianakis <antoprinia@gmail.com>
2019-03-24 00:28:04 +0200

    initialize kai constructor

--- symtable.hpp
+++ symtable.hpp
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <string.h>
+#include <iostream>
 #include <string>
 #include <vector>
+
+#define HASH_MUL 65599
 
 using namespace std;
 
@@ -37,7 +40,6 @@ typedef struct SymbolTableEntry {
   SymbolTableEntry *next;
   SymbolTableEntry *scope_next;
 } SymbolTableEntry;
-
 
 class SymTable {
  private:
@@ -81,20 +83,37 @@ class SymTable {
   }
 
  public:
-  SymTable() { initialize(); }
+  SymTable() {
+    symbol_table = new SymbolTableEntry *[100];
+
+    initialize();
+  }
 
   void initialize() {
     // init scopes
+    scope = 0;
+    func_scope = 0;
+
     // insert all libfuncs
+    insert("sin", 0, LIBFUNC);
+    insert("cos", 0, LIBFUNC);
+    insert("sqrt", 0, LIBFUNC);
+    insert("print", 0, LIBFUNC);
+    insert("input", 0, LIBFUNC);
+    insert("typeof", 0, LIBFUNC);
+    insert("argument", 0, LIBFUNC);
+    insert("strtonum", 0, LIBFUNC);
+    insert("objectcopy", 0, LIBFUNC);
+    insert("totalarguments", 0, LIBFUNC);
+    insert("objectmemberkeys", 0, LIBFUNC);
+    insert("objecttotalmembers", 0, LIBFUNC);
   }
 
-  static unsigned int SymTable_hash(const char *pcKey) {
-    // size_t ui;
-    // unsigned int uiHash = 0U;
-    // for (ui = 0U; pcKey[ui] != '\0'; ui++)
-    //   uiHash = uiHash * HASH_MULTIPLIER + pcKey[ui];
-    // return uiHash % buckets;
-    return 0;
+  static unsigned int SymTable_hash(const char *name) {
+    size_t ui;
+    unsigned int uiHash = 0U;
+    for (ui = 0U; name[ui] != '\0'; ui++) uiHash = uiHash * HASH_MUL + name[ui];
+    return uiHash % 100;
   }
 
   // enum SymbolType { GLOBAL, LOCAL, FORMAL, USERFUNC, LIBFUNC };
@@ -130,15 +149,14 @@ class SymTable {
 
     if (myscope > scopes.size() + 1) {
       // error case
-    }else if(myscope == scopes.size()){ //add new level of scopes
+    } else if (myscope == scopes.size()) {  // add new level of scopes
       newnode->scope_next = NULL;
-    }else{ //connect to current scope list
+    } else {  // connect to current scope list
       newnode->scope_next = scopes.at(myscope);
     }
-    scopes.insert( scopes.begin() + myscope ,newnode);
+    scopes.insert(scopes.begin() + myscope, newnode);
 
     size++;
-
   }
 
   /* local x; function f() calls this
@@ -168,7 +186,7 @@ class SymTable {
 
     return declared;
   }
-  //  x 
+  //  x
   int lookUp_allscope(const char *name, SymbolType symtp) {
     SymbolTableEntry *curr = symbol_table[SymTable_hash(name)];
     int declared = 0;
@@ -180,10 +198,9 @@ class SymTable {
       // print  libfunc error
       if (curr->type = LIBFUNC) return -1;
 
-      if(scope <= get_scope(curr) && scope >= min_scope ){
-        if (is_var(symtp)) 
-          declared = 1;
-        
+      if (scope <= get_scope(curr) && scope >= min_scope) {
+        if (is_var(symtp)) declared = 1;
+
         // name refers to previous declaration of func/ no need to insert
         else
           declared = 1;
@@ -204,4 +221,3 @@ class SymTable {
     return 0;
   }
 };
-
